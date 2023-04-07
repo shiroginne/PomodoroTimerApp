25164380a67bef104d168cd7b77ae568463562f2
Yauheni Suhakou <shiroginne@gmail.com>
2023-04-07 10:35:25 +0200

    Initial Commit

---
+++ PomodoroTimerApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  PomodoroTimerApp
+//
+//  Created by Yauheni Suhakou on 07.04.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PomodoroTimerApp/PomodoroTimerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomodoroTimerAppApp.swift
+//  PomodoroTimerApp
+//
+//  Created by Yauheni Suhakou on 07.04.23.
+//
+
+import SwiftUI
+
+@main
+struct PomodoroTimerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
